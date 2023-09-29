96477ec15ff93a151d983c381defe4a88bd95665
Stanislau Linnik <lstas102938@gmail.com>
2023-09-29 23:42:23 +0300

    CalcLogic

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,36 +4,55 @@ string? symb;
 while (true) { 
 	try
 	{
+
+		bool ZeroCheck(double num, string? symb)
+		{
+			if(num == 0 && symb == "/")
+			{
+				return true;
+			}
+			return false;
+		}
+
+		bool Calc(double num1, double num2, string? symb)
+		{
+            switch (symb)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели недопустимый символ операции");
+					return true;
+
+            }
+			return false;
+        }
+
 		Console.WriteLine("Введите первое число: ");
 		num1 = Convert.ToDouble(Console.ReadLine());
 		Console.WriteLine("Введите второе число: ");
 		num2 = Convert.ToDouble(Console.ReadLine());
 		Console.WriteLine("Введите символ операции: ");
 		symb = Convert.ToString(Console.ReadLine());
-		if(num2 == 0 && symb == "/")
+		if(ZeroCheck(num2, symb))
 		{
 			Console.WriteLine("Делить на 0 нельзя!");
 			continue;
 
 		}
-		switch (symb)
+		if(Calc(num1, num2, symb))
 		{
-			case "+":
-				Console.WriteLine(num1 + num2);
-				break;
-			case "-":
-				Console.WriteLine(num1 - num2);
-				break;
-			case "*":
-				Console.WriteLine(num1 * num2);
-				break;
-			case "/":
-				Console.WriteLine(num1 / num2);
-				break;
-			default:
-				Console.WriteLine("Вы ввели недопустимый символ операции");
-				continue;
-
+			continue;
 		}
 		break;
 	}
